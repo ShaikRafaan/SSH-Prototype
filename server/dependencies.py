97f94261645accd95230d63612adc2e180b117d4
ShaikRafaan <shaik.rafaan@gmail.com>
2024-12-06 19:43:42 +0400

    Updated dependencies

--- server/dependencies.py
+++ server/dependencies.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from .database import SessionLocal
+from .database import LocalSession
 #Fetch a new database session for asynchronus database operations
 async def f_database_session():
-    async with SessionLocal() as database_session:
+    async with LocalSession() as database_session:
         yield database_session
