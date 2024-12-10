78b1e14ee964f0b4fa53d55997d119b81622d924
sasavatt11 <sasavatt@gmail.com>
2024-12-10 19:12:09 +0400

    made changes to column fields in accommodation.py

--- server/models/accommodations.py
+++ server/models/accommodations.py
@@ -4,8 +4,8 @@ from .base_case import base_case as Base
 class Accommodation(Base):
     __tablename__ = "accommodations"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    address = Column(String, nullable=False)
-    city = Column(String, nullable=False)
-    contact_number = Column(String, nullable=False)
+    id = Column(Integer(15), primary_key=True, unique=True, nullable=False)
+    name = Column(String(20), nullable=False)
+    address = Column(String(40), nullable=False)
+    city = Column(String(15), nullable=False)
+    contact_number = Column(String(15), nullable=False, unique=True)
