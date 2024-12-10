06e2e8443bd232e911fbec35fdb748ae726f1d87
ShaikRafaan <shaik.rafaan@gmail.com>
2024-12-10 19:05:02 +0400

    Product model

---
+++ server/models/product.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from server.models.base_case import  base_case as Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True,unique=True)
+    name = Column(String, index=True,unique=True)
+    price = Column(Float)
+    in_stock = Column(Boolean, default=True)
+    supermarket_id = Column(Integer, ForeignKey("supermarkets.id"))
+
+    supermarket = relationship("Supermarket", back_populates="products")
