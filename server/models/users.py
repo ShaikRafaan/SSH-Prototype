dbdaa33786aee6982f38aea83bcf6af43aaa4b01
sasavatt11 <sasavatt@gmail.com>
2024-12-10 19:12:52 +0400

    changes to fields to columns in users.py

--- server/models/users.py
+++ server/models/users.py
@@ -4,7 +4,8 @@ from .base_case import base_case as Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    id = Column(Integer(15), primary_key=True, index=True)
+    name = Column(String(30), nullable=False)
+    email = Column(String(30), unique=True, nullable=False)
+    password = Column(String(20), nullable=False)
+    accommodation_id = Column(String(15), foreign_key=True, nullable=False)
