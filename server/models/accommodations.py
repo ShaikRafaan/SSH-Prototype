36fa3965b195d9f39ceeacef5712e811b8b4b1fe
sasavatt11 <sasavatt@gmail.com>
2024-12-10 19:22:52 +0400

    fixed small error in accomodations.py

--- server/models/accommodations.py
+++ server/models/accommodations.py
@@ -4,7 +4,7 @@ from .base_case import base_case as Base
 class Accommodation(Base):
     __tablename__ = "accommodations"
 
-    id = Column(Integer(15), primary_key=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     name = Column(String(20), nullable=False)
     address = Column(String(40), nullable=False)
     city = Column(String(15), nullable=False)
