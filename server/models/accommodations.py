fb4560230971bde052d01a781f726d64be1edbd4
sasavatt11 <sasavatt@gmail.com>
2024-12-11 13:01:53 +0400

    added a field to accommodations.py

--- server/models/accommodations.py
+++ server/models/accommodations.py
@@ -5,7 +5,8 @@ class Accommodation(Base):
     __tablename__ = "accommodations"
 
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
-    name = Column(String(20), nullable=False)
+    first_name = Column(String(20), nullable=False, unique=False)
+    last_name = Column(String(20), nullable=False, unique=False)
     address = Column(String(40), nullable=False)
     city = Column(String(15), nullable=False)
     contact_number = Column(String(15), nullable=False, unique=True)
