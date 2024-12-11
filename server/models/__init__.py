0eeebb6ce3f2953b3627f5cc9acd55df60097118
sasavatt11 <sasavatt@gmail.com>
2024-12-11 13:22:35 +0400

    added the hardcoded data for accommodations and users to init.py

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,3 +1,3 @@
 from .base_case import base_case
-from .users import User
-from .accommodations import Accommodation
+from .users import User, hardcoded_users
+from .accommodations import Accommodation, hardcoded_accommodations
