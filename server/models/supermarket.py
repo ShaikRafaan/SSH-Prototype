03707c5ade5a548e33be7bcc2b4f35dc95ba6433
sasavatt11 <sasavatt@gmail.com>
2024-12-11 12:48:42 +0400

    Added a field to supermarket.py

--- server/models/supermarket.py
+++ server/models/supermarket.py
@@ -4,5 +4,6 @@ from server.models.base_case import base_case as Base
 class Supermarket(Base):
     __tablename__ = "supermarkets"
     id = Column(Integer, primary_key=True,unique=True)
-    name = Column(String(20),unique=True)
+    first_name = Column(String(20),unique=False)
+    last_name= Column(String(20), unique=False)
     location = Column(String(40), unique=False)
